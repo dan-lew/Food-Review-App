7f67a70cf4492c9424f7665c796730eb1870f5ce
raniaw <raniaw@web.de>
2020-03-25 22:28:15 +0100

    slider changed

--- client/src/MapComponents/ConstraintSlider.js
+++ client/src/MapComponents/ConstraintSlider.js
@@ -1,23 +1,16 @@
 import React from "react";
 import { Slider } from "antd";
 import { CarTwoTone } from "@ant-design/icons";
-import { MuiThemeProvider } from "material-ui";
-import getMuiTheme from "material-ui/styles/getMuiTheme";
+
 
 const ConstraintSlider = ({ iconType, value, onChange, text }) => {
-  const muiTheme = getMuiTheme({
-    slider: {
-      trackColor: "yellow",
-      selectionColor: "red"
-    }
-  });
-console.log(muiTheme.slider)
+
   return (
     <section className="">
       <div className="">
         {/* <Icon className="" type={iconType} /> */}
         <CarTwoTone style={{ fontSize: "25px" }} twoToneColor="#9c27b0" />
-        <MuiThemeProvider muiTheme={muiTheme}>
+       
           <Slider
             className=""
             value={value}
@@ -25,7 +18,7 @@ console.log(muiTheme.slider)
             max={60}
             onChange={onChange}
           />
-        </MuiThemeProvider>
+   
       </div>
       <span className="text-center">{text}</span>
     </section>
